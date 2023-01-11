62c47eedafb4b6adaaadadeea4ef76de83ebdefa
Ismail <ismailbak7@gmail.com>
2023-01-11 13:52:55 +0100

    adding spring boot project 🍃

---
+++ backend/src/main/java/com/ismail/personalblogpost/PersonalblogpostApplication.java
@@ -0,0 +1,13 @@
+package com.ismail.personalblogpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalblogpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalblogpostApplication.class, args);
+	}
+
+}
