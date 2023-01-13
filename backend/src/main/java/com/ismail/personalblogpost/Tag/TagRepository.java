c0240d2499ab756ad712aff81b7b90baa803051a
Ismail <ismailbak7@gmail.com>
2023-01-13 12:11:10 +0100

    Add Tag Repository

---
+++ backend/src/main/java/com/ismail/personalblogpost/Tag/TagRepository.java
@@ -0,0 +1,9 @@
+package com.ismail.personalblogpost.Tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag,Long> {
+
+}
